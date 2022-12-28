5241819755bf4e7b89a00b3c9974de0da41623f3
aureljoss <josserand.n.aurelie@gmail.com>
2022-12-28 12:20:10 -0500

    Sets up js and reset button

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const bill=document.getElementById('bill');
+const tipSelected=document.querySelector('.default-button');
+const defaultTwenty=document.getElementById('twenty');
+const custom=document.getElementById('custom');
+
+const people=document.getElementById('people');
+
+const tipAmount=document.getElementById('tip-amount');
+const totalPerson=document.getElementById('total');
