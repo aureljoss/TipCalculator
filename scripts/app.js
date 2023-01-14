cb8d444f782fcbfea9ac5df45fed5404887af890
aureljoss <josserand.n.aurelie@gmail.com>
2023-01-13 21:48:20 -0500

    Sets up tip calculations

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,20 @@
-const bill=document.getElementById('bill').value;
-const tipSelected=document.querySelector('.default-button');
-const defaultTwenty=document.getElementById('twenty');
-const custom=document.getElementById('custom');
+const bill = document.getElementById("bill");
+const tipSelected = document.querySelector(".default-button");
+const defaultTwenty = document.getElementById("twenty");
+const custom = document.getElementById("custom");
 
-const people=document.getElementById('people');
+const tipAmount = document.getElementById("tip-amount");
+const totalPerson = document.getElementById("total");
 
-const tipAmount=document.getElementById('tip-amount').value;
-const totalPerson=document.getElementById('total');
+let billTotal = 0;
+let tipPercentage = 0;
+let peopleTotal = 0;
 
-let billTotal=0;
-let tipPercentage=0;
-let peopleTotal=0;
+const twenty = () => {
+      // Selecting the input element and get its value 
+    var tipAmount = document.getElementById("bill").value;
+    const people = document.getElementById("people").value;
+    document.getElementById("tip-amount").value=((tipAmount*0.2)/people);
+  
+    // Displaying the value
+    alert(inputVal);}
