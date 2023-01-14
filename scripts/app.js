571481f6146de0fa7561f57188c7d9980b469e45
aureljoss <josserand.n.aurelie@gmail.com>
2023-01-14 11:37:39 -0500

    Updates calculation function and html values

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,21 @@
-const bill = document.getElementById("bill");
 const tipSelected = document.querySelector(".default-button");
 const defaultTwenty = document.getElementById("twenty");
 const custom = document.getElementById("custom");
-
-const tipAmount = document.getElementById("tip-amount");
-const totalPerson = document.getElementById("total");
 
 let billTotal = 0;
 let tipPercentage = 0;
 let peopleTotal = 0;
 
-const twentyTip = () => {
+function totals(tip) {
   // Selecting the input elements and get their value
-  var bill = document.getElementById("bill").value;
+  const bill = document.getElementById("bill").value;
   const people = document.getElementById("people").value;
 
   // Calculates tip and total per person
-  var tipAmount = (bill * 0.2) / people;
-  var totalPerson = tipAmount + bill / people;
+  const tipAmount = (bill * tip) / people;
+  const totalPerson = tipAmount + bill / people;
 
   // Pushes the calculated amounts to inputs
-  document.getElementById("tip-amount").value = tipAmount;
-  document.getElementById("total").value = totalPerson;
-};
+  document.getElementById("tip-amount").value = "$" + tipAmount.toFixed(2);
+  document.getElementById("total").value = "$" + totalPerson.toFixed(2);
+}
