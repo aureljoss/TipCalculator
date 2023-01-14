46c3de308f8b64d6c713fdf1a6e39776851e5d81
aureljoss <josserand.n.aurelie@gmail.com>
2023-01-14 13:50:46 -0500

    Adds warning display properties

--- scripts/app.js
+++ scripts/app.js
@@ -23,13 +23,25 @@ function totals(tip) {
   // Displays warning for invalid inputs
   if (people < 1) {
     document.getElementById("warnings").innerHTML = "Can't be 0";
+    document.getElementById("tip-amount").value = "$0.00";
+    document.getElementById("total").value = "$0.00";
+    document.getElementById("people").style.border = "2px solid red";
   } else if (people > 10) {
     document.getElementById("warnings").innerHTML = "Can't be more than 10";
+    document.getElementById("tip-amount").value = "$0.00";
+    document.getElementById("total").value = "$0.00";
+    document.getElementById("people").style.border = "2px solid red";
   }
   if (bill < 1) {
     document.getElementById("billWarning").innerHTML = "Can't be 0";
+    document.getElementById("tip-amount").value = "$0.00";
+    document.getElementById("total").value = "$0.00";
+    document.getElementById("bill").style.border = "2px solid red";
   } else if (bill > 1000) {
     document.getElementById("billWarning").innerHTML =
       "Can't be more than $1,000";
+    document.getElementById("tip-amount").value = "$0.00";
+    document.getElementById("total").value = "$0.00";
+    document.getElementById("bill").style.border = "2px solid red";
   }
 }
